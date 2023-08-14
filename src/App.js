ba14cd3482c46d29cf764a7b90389de7b4154940
LightsidE <ojeleyeprecious@gmail.com>
2023-08-14 09:36:37 +0100

    made changes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import DriversSignup from "./pages/Drivers page/DriversSignup";
 import LoginPage from "./pages/Passengers Page/LoginPage";
 import SignUpPage from "./pages/Passengers Sign Up Page/SignUp";
 import ForgottenPassword from "./pages/ForgottenPassword/ForgottenPassword";
+import AboutScreen from "./pages/AboutScreen/AboutScreen";
+import NewPassword from "./pages/NewPassword/NewPassword";
 
 function App() {
   return (
@@ -16,6 +18,9 @@ function App() {
       <Route path='/Login' element={<LoginPage/>}/>
       <Route path='/SignUp' element={<SignUpPage/>}/>
       <Route path='/forgotten-password' element={<ForgottenPassword/>}/>
+      <Route path='/about' element = {<AboutScreen/>}/>
+      <Route path='/new-password' element = {<NewPassword/>}/>
+
     </Routes>
    
   );
---
+++ src/pages/NewPassword/NewPassword.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './NewPassword.css'
+import PasswordInput from "../../components/Password/Password"
+
+const NewPassword = () => {
+  return (
+    <div className='NewPassword'>
+        <div className='BlueContainer'>
+            <div className='WhiteContainer'>
+                <a href=""><img className='arrow-left' src="/Assets/arrow-left.svg" alt="arrow-left" /></a>
+            </div>
+            <p className='New'>New Password</p>
+        </div>
+
+        <div className='Whitebox'>
+            <p className='text'>New Password</p>
+            <p className='text2'>Please enter your new password</p>
+
+        <div>
+        <form action="">
+            <div className='InputFields'>
+                <label for="email" className='labelpass'>Password</label>
+                    <PasswordInput/>
+            </div>
+            <div className='InputFields'>
+                <label for="email" className='labelpass2'>Confirm Password</label>
+                    <PasswordInput/>
+            </div>
+                <input className='SendBtn' type="submit" value="Confirm Password"/>
+        </form>
+        </div>
+        {/* <div className='bottomtext'>
+            <p className='text3'><span className='browntext'>Remember Password?</span> <span className='bluetext'><a href="">Log in</a></span></p>
+        </div> */}
+        </div>        
+
+    </div>
+  )
+}
+
+export default NewPassword
