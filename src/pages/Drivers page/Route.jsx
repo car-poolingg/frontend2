50a3e50d0bf93f79a1270121df147cde2bab6a4d
Franklynstein <95643509+Franklynstein@users.noreply.github.com>
2023-08-16 00:19:13 +0100

    anything

---
+++ src/pages/Drivers page/Route.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Icon } from '@iconify/react';
+import "../../bootstrap.min.css"
+// import './Styles.scss';
+import "./DriversPage.css"
+
+
+const Route = () => {
+  return (
+    <div>Route</div>
+  )
+}
+
+export default Route
