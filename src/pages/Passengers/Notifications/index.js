b870492c73d07190acc156529610c473accce13c
Dubjay18 <jejeniyi7@gmail.com>
2023-08-15 17:32:17 +0100

    feat:notifications

---
+++ src/pages/Passengers/Notifications/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Header from "../../../components/global/Header/Header";
+import "./Notifications.css";
+import { Card } from "react-bootstrap";
+import NotificationCard from "../../../components/passengers/NotificationCard";
+
+function Notifications() {
+  return (
+    <div className='notifications-page-container'>
+      <Header title={"Notifications"} menuType={true} />
+      <div className='p-3'>
+        <h5>Today</h5>
+        <NotificationCard
+          username={"James Onigidi"}
+          message={"sent you a request."}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Notifications;
