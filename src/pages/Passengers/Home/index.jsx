4813981f4c6d574ad2d52dfd75d0359da47e20a6
Dubjay18 <jejeniyi7@gmail.com>
2023-08-16 20:14:39 +0100

    feat:map in passenger homescreen

--- src/pages/Passengers/Home/index.jsx
+++ src/pages/Passengers/Home/index.jsx
@@ -5,10 +5,13 @@ import Profile from "../Profile";
 import Notifications from "../Notifications";
 import OngoingRides from "../Rides/Ongoing";
 import FindRide from "../Rides/Find";
+import Header from "../../../components/global/Header/Header";
+import HomeScreen from "./HomeScreen";
 
 function PassengerHome() {
   return (
     <Routes>
+      <Route path='/' element={<HomeScreen />} />
       <Route
         path='availableDrivers'
         element={<AvailableDrivers />}
