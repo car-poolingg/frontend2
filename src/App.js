9e3c187dc58e4f0eeccdcffa49c6b6bea22a92bb
Franklynstein <95643509+Franklynstein@users.noreply.github.com>
2023-08-16 15:07:06 +0100

    anything

--- src/App.js
+++ src/App.js
@@ -10,20 +10,23 @@ import AboutScreen from "./pages/AboutScreen/AboutScreen";
 import NewPass from "./pages/NewPassword/NewPassword";
 import PersonalDetails from "./pages/Drivers page/PersonalDetails";
 import Document from "./pages/Drivers page/Document";
+import IdentityVerification from "./pages/Drivers page/IdentityVerification";
+import VehicleVerification from "./pages/Drivers page/VehicleVerification";
 import License from "./pages/Drivers page/License";
 import Support from "./pages/Drivers page/Support";
 import DriversProfile from "./pages/Drivers page/DriversProfile";
 import ForgotPassword from "./pages/Drivers page/ForgotPassword";
 import VerifyPassword from "./pages/Drivers page/VerifyPassword";
-import NewPassword from "./pages/Drivers page/NewPassword";
 import VerificationInput from "./pages/OTPVerification/OTPVerification";
 import EditProfile from "./pages/Drivers page/EditProfile";
 import AvailableDrivers from "./pages/Passengers/AvailableDrivers";
+import OngoingRide from "./pages/Drivers page/OngoingRide"
 import Profile from "./pages/Passengers/Profile";
 import PassengerHome from "./pages/Passengers/Home";
 import ProfilePage from "./pages/Drivers page/ProfilePage";
 import RatePassenger from "./pages/Drivers page/RatePassenger";
 import RatingDriver from "./pages/Drivers page/RatingDriver";
+
 
 
 function App() {
@@ -38,21 +41,23 @@ function App() {
       <Route path='/new-password' element={<NewPass />} />
       <Route path='/PersonalDetails' element={<PersonalDetails />} />
       <Route path='/Document' element={<Document />} />
+      <Route path='/IdentityVerification' element={<IdentityVerification />} />
+      <Route path='/VehicleVerification' element={<VehicleVerification />} />
       <Route path='/License' element={<License />} />
       <Route path='/Support' element={<Support />} />
       <Route path='/DriversProfile' element={<DriversProfile />} />
       <Route path='/ForgotPassword' element={<ForgotPassword />} />
       <Route path='/VerifyPassword' element={<VerifyPassword />} />
       <Route path='/ProfilePage' element={<ProfilePage />} />
-      <Route path='/NewPassword' element={<NewPassword />}  />
       <Route path='/EditProfile' element={<EditProfile />} />
       <Route path='/passengers/*'element={<PassengerHome />}  />
       <Route path='/AvailableDrivers' element={<AvailableDrivers />} />
+      <Route path = '/OngoingRide' element={<OngoingRide/>} />
       <Route path='/verifycode' element={<VerificationInput />} />
       <Route path='/RatePassenger' element={<RatePassenger />} />
       <Route path='/RatingDriver' element={<RatingDriver />} />
       <Route path='/Profile' element={<Profile />} />
-      <Route
+          <Route
         path='/DriversProfile'
         element={<DriversProfile />}
       />
@@ -68,10 +73,7 @@ function App() {
         path='/VerifyPassword'
         element={<VerifyPassword />}
       />
-      <Route
-        path='/NewPassword'
-        element={<NewPassword />}
-      />
+      
       <Route
         path='/EditProfile'
         element={<EditProfile />}
---
+++ src/pages/Drivers page/VehicleVerification.jsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import { Icon } from '@iconify/react';
+import "../../bootstrap.min.css"
+// import './Styles.scss';
+import "./DriversPage.css";
+import FileChooser from '../../components/Upload/uploadfile';
+
+const VehicleVerification = () => {
+  return (
+    <div>
+    <div className='NavTop'>
+<div className='iconEllipse'>
+<Icon icon="material-symbols:arrow-back" color="#0c3ead" height={"24px"} width={"24px"} />
+</div>
+<h6 className='DetailsNavTopText'>Verify your Vehicle</h6>
+</div>
+
+<div className='DriversDetails'>
+        <div className='DocumentUpload'>
+        <h4 className='DocumentText'>Vehicle Inspection Policy</h4>
+        <p className='DocumentSubText'>Upload a snapshot of your vehicle inspection policy</p>
+        {/* <button className='Uploadbutton'>+Upload file</button> */}
+        <FileChooser/>
+
+        <hr />
+
+    </div>
+    <div className='DocumentUpload'>
+        <h4 className='DocumentText'>Vehicle Insurance Policy</h4>
+        <p className='DocumentSubText'>Upload a snapshot of your vehicle insurance policy</p>
+        {/* <button className='Uploadbutton'>+Upload file</button> */}
+        <FileChooser/>
+
+        <hr />
+
+    </div>
+    <div className='DocumentUpload'>
+        <h4 className='DocumentText'>Front photo of your car</h4>
+        <p className='DocumentSubText'>Upload a clear front photo that captures the plate number.</p>
+        {/* <button className='Uploadbutton'>+Upload file</button> */}
+        <FileChooser/>
+
+        <hr />
+
+    </div>
+    <div className='DocumentUpload'>
+        <h4 className='DocumentText'>Back photo of your car</h4>
+        <p className='DocumentSubText'>Upload a clear back photo that captures the plate number.</p>
+        {/* <button className='Uploadbutton'>+Upload file</button> */}
+        <FileChooser/>
+
+
+        <hr />
+
+    </div>
+    <div className='DocumentUpload'>
+        <h4 className='DocumentText'>Interior Photo of your car</h4>
+        <p className='DocumentSubText'>Upload a clear interior photo of your car</p>
+        {/* <button className='Uploadbutton'>+Upload file</button> */}
+        <FileChooser/>
+
+        <hr style={{backgroundColor: 'black'}} />
+
+    </div>
+    <input className='SignUpBtn' type="submit" value="Next" />
+</div>
+
+</div>
+  )
+}
+
+export default VehicleVerification
