3fe36c1c49cab9d70b03435bd35d7312dd909627
xpan <xpanvictor@gmail.com>
2023-08-17 05:56:16 +0100

    feat: notifications using interval:

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,11 +2,15 @@ import axios from "axios";
 import { getData } from "./api.storage";
 import StorageConstants from "./constants.storage";
 
+const baseURL = "https://tag-c5ba886bbc3c.herokuapp.com/api/v1"
+
 const axiosInstance = axios.create({
-    baseURL: "https://tag-c5ba886bbc3c.herokuapp.com/api/v1",
+    baseURL,
 })
 
-export const authAxiosInstance = axiosInstance({
+// could use interceptor but rushed
+export const authAxiosInstance = axios.create({
+    baseURL,
     headers: {
         Authorization: `Bearer ${getData(StorageConstants.Token).data}`
     }
